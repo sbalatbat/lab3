c8dcb6a8206bf82869e7ae7f12c6ea31d1f0941d
scbalatbat <scsb1119@gmail.com>
2023-01-26 16:57:32 -0800

    Fixed bugs

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,19 +4,23 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+    int[] newArray = new int[arr.length];
+    for(int i = 0, j = arr.length-1; j >= 0; i++, j--) {
+      newArray[i] = arr[j];
     }
+    for(int i = 0; i < arr.length; i++) {
+      arr[i] = newArray[i];
+    } 
   }
-
+  
   // Returns a *new* array with all the elements of the input array in reversed
   // order
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,23 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
+  
 	@Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
+    int [] input2 = {0, 1, 2};
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{2, 1, 0}, input2);
 	}
-
 
   @Test
   public void testReversed() {
     int[] input1 = { };
+    int [] input2 = {0, 1, 2};
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    assertArrayEquals(new int[]{2, 1, 0}, ArrayExamples.reversed(input2));
   }
+
 }
